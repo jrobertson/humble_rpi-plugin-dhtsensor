b46ae324b6c325ecc0ad327f7cc54d83ea05bd7c
Your Name <you@example.com>
2016-02-20 13:26:45 +0000

    1st commit

---
+++ humble_rpi-plugin-dhtsensor.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'humble_rpi-plugin-dhtsensor'
+  s.version = '0.1.0'
+  s.summary = 'A Humble RPi plugin to read the ambient temperature and humidity from the DHT sensor.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/humble_rpi-plugin-dhtsensor.rb']
+  s.add_runtime_dependency('dht-sensor-ffi', '~> 0.1', '>=0.1.4')  
+  s.signing_key = '../privatekeys/humble_rpi-plugin-dhtsensor.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@r0bertson.co.uk'
+  s.homepage = 'https://github.com/jrobertson/humble_rpi-plugin-dhtsensor'
+end
---
+++ lib/humble_rpi-plugin-dhtsensor.rb
@@ -0,0 +1,73 @@
+#!/usr/bin/env ruby
+
+# file: humble_rpi-plugin-dhtsensor.rb
+
+
+require 'dht-sensor-ffi'
+
+
+class HumbleRPiPluginDhtSensor
+  
+
+  def initialize(settings: {}, variables: {})
+
+    @pin = settings[:pin]
+    
+    if @pin.nil? then
+      raise 'HumbleRPiPluginDhtSensor: settings error: please supply a pin'
+    end
+    
+    h = {
+      version: 11,
+      temperature_threshold: 1,
+      humidity_threshold: 2,
+      interval: 3,
+      msg_format: "%s/dht11_sensor: temperature: %s humidity: %s"
+    }.merge settings
+    
+    @version = h[:version]
+    @temperature_threshold = h[:temperature_threshold]
+    @humidity_threshold = h[:humidity_threshold]
+    @interval = h[:interval]
+    @msg_format = h[:msg_format]
+    
+    @notifier = variables[:notifier]
+    @device_id = variables[:device_id] || 'pi'
+    
+  end
+
+  def start()
+    
+    puts 'ready to monitor temperature and humidity'
+    
+    val = DhtSensor.read(@pin, @version)
+    push val
+    
+    loop do
+      
+    val = DhtSensor.read(@pin, @version)
+
+      if (@old_tval - val.temperature).abs >= @temperature_threshold or \
+                     (@old_hval - val.humidity).abs >= @humidity_threshold then
+        push val
+      end      
+
+      sleep @interval
+      
+    end
+          
+  end
+  
+  alias on_start start
+
+  private
+  
+  def push(val)
+    
+    @notifier.notice @msg_format % [@device_id, val.temperature, val.humidity]
+    @old_tval = val.temperature
+    @old_hval = val.humidity        
+    
+  end
+  
+end
